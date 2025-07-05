adb68246fbafa40241460ae572b3e2e615afdaf2
Jose7RT <jose7rt@gmail.com>
2025-07-05 13:56:19 +0200

    Añadido el SDK de sensores de WE para soportar el sensor WSEN PADS (barómetro). Se incluyen los 2 ejemplos presentes en el repositorio del SDK preparados para correr en la familia STM32.
    En el fichero platform.c se incluye el código específico de la plataforma (STM32), que implementa la comunicación con la SDK del sensor empleado funciones de la STM32 HAL.
    
    El ejemplo dentro del directorio WSEN_PADS implementa un uso básico del sensor de presión absoluta, PADS,  conectado a través de I2C.
    
    El ejemplo dentro del directorio WSEN_PADS_ADVANCED implementa un uso avanazado del sensor de presión absoluta, PADS, conectado a través de I2C (interrupción de datos listos, modo AUTOZERO, uso del buffer FIFO).
    
    El correcto funcionamiento del código no hasido probado en una placa de desarrollo por ausencia del sensor.

---
+++ examples/main.c
@@ -0,0 +1,24 @@
+#include "i2c.h"
+#include "usart.h"
+#include "gpio.h"
+
+#include "WSEN_PADS_EXAMPLE.h"
+
+void SystemClock_Config(void);
+
+int main(void)
+{
+  HAL_Init();
+  SystemClock_Config();
+
+  MX_GPIO_Init();
+  MX_I2C1_Init();
+  MX_USART2_UART_Init();
+
+  WE_padsExampleInit();
+
+  while (1)
+  {
+    WE_padsExampleLoop();
+  }
+}
